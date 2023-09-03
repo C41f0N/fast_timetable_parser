a87bcfe0b9cf058a3dd4b3ba8725988416c0a157
Sarim Ahmed <C41f0N@protonmail.com>
2023-09-03 15:48:36 +0500

    can parse and read all slot accurately

--- class_object.py
+++ class_object.py
@@ -1,11 +1,35 @@
+import re
+
+
 class Class:
-    def __init__(self, time_slot, instructor, course, room, weekday):
+    def __init__(self, time_slot, instructor, course, room, weekday, index):
         self.time_slot = time_slot
-        self.instrucor = instructor
+        self.instructor = instructor
         self.course = course
         self.room = room
         self.weekday = weekday
+        self.index = index
+
+    def has_in_course(self, pattern):
+        return pattern in self.course
 
 
-def isValidClass(classDataString):
+def isValidClassData(classDataString):
     return str(classDataString).lstrip().rstrip().find("\n") != -1
+
+
+def isLabClassData(classDataString):
+    return "LAB" in re.split("\s|-", str(classDataString))
+
+
+def displayClassObjectList(classObjectList):
+    for classSlot in classObjectList:
+        if classSlot.has_in_course("BCS-1D"):
+            print("=================================================")
+            print(classSlot.course)
+            print(classSlot.time_slot)
+            print(classSlot.instructor)
+            print(classSlot.room)
+            print(classSlot.weekday)
+            print(classSlot.index)
+            print("=================================================\n\n")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from class_object import isValidClass
+import class_object
+from class_object import Class
 
 fileName = "timetable.xlsx"
 
@@ -29,13 +30,88 @@ for sheet in timeTable.sheet_names:
     if is_weekday(sheet):
         print(f"*** DATA FOR {sheet.upper()} ***")
 
+        dayClasses = []
+
+        # Read complete excel sheet including the title columns/rows
+        completeExcelSheet = timeTable.parse(sheet)
+        completeExcelSheetArray = completeExcelSheet.to_numpy()
+
+        # Read excel sheet excluding the title columns/rows
         dayTimeTable = timeTable.parse(sheet, header=3, index_col=0)
         classesArray = dayTimeTable.to_numpy()
 
-        for row in classesArray:
-            for classData in row:
-                print("\n")
-                print(classData)
-                print(isValidClass(classData))
+        # loop over every cell
+        for i in range(len(classesArray)):
+            row = classesArray[i]
+            for j in range(len(row)):
+                classData = row[j]
 
+                # if the current slot is a valid class
+                if class_object.isValidClassData(classData):
+                    # reading slot time and classroom and slot number(index)
+                    classroom = completeExcelSheetArray[i + 3][0]
+                    timeSlot = completeExcelSheetArray[1][j + 1]
+                    index = completeExcelSheetArray[0][j + 1]
+
+                    # extracting more info from the class
+                    instructor = str(classData).split("\n")[1].rstrip().lstrip()
+                    course = str(classData).split("\n")[0].rstrip().lstrip()
+
+                    # this part accounts for the merged cells for lab classes
+
+                    # checking if current cell is a lab class
+                    if class_object.isLabClassData(classData):
+                        # Create the next three class slots instead of one
+
+                        # get the timeslot for next two slots
+                        timeSlot2 = completeExcelSheetArray[1][(j + 1) + 1]
+                        timeSlot3 = completeExcelSheetArray[1][(j + 1) + 2]
+
+                        labClass1 = currentClass = Class(
+                            time_slot=timeSlot,
+                            instructor=instructor,
+                            room=classroom,
+                            weekday=sheet,  # sheetname is weekday
+                            index=index,
+                            course=course,
+                        )
+
+                        labClass2 = currentClass = Class(
+                            time_slot=timeSlot2,
+                            instructor=instructor,
+                            room=classroom,
+                            weekday=sheet,  # sheetname is weekday
+                            index=index + 1,
+                            course=course,
+                        )
+
+                        labClass3 = currentClass = Class(
+                            time_slot=timeSlot3,
+                            instructor=instructor,
+                            room=classroom,
+                            weekday=sheet,  # sheetname is weekday
+                            index=index + 2,
+                            course=course,
+                        )
+
+                        dayClasses.append(labClass1)
+                        dayClasses.append(labClass2)
+                        dayClasses.append(labClass3)
+
+                        pass
+
+                    else:
+                        # Create a class object using the classData
+                        currentClass = Class(
+                            time_slot=timeSlot,
+                            instructor=instructor,
+                            room=classroom,
+                            weekday=sheet,  # sheetname is weekday
+                            index=index,
+                            course=course,
+                        )
+
+                        dayClasses.append(currentClass)
+
+        class_object.displayClassObjectList(dayClasses)
         break
