dd5f9e13e9adaed04583ccb1d637a130a2181554
Raahim Irfan <94684550+raahimirfan100@users.noreply.github.com>
2024-02-01 11:41:03 +0500

    Create fetch.py
    added the functionality to automatically import the lastest version of the timetable directly into the project.

---
+++ fetch.py
@@ -0,0 +1,17 @@
+import gdown
+import pandas as pd
+
+google_sheets_url = 'https://docs.google.com/spreadsheets/d/1H3zQ80ptZTc1cI60mt9oWkAa2_JBKqebGflFATf3nvI/'
+
+# Get the file ID from the Google Sheets URL
+file_id = google_sheets_url.split('/')[-2]
+print(file_id)
+
+# Construct the download link
+download_link = f'https://drive.google.com/uc?id={file_id}'
+
+# Define the output file name
+output_file = 'timetable.xlsx'
+
+# Download the file using gdown
+gdown.download(download_link, output_file, quiet=False)
